994e3f2bcf18df81d2ccb3e7b7fbe5e5951832fd
newtonhaven <dervissami@hotmail.com>
2022-08-31 15:55:12 +0300

    calculator added

--- C++ Challenges/Calculator.cpp
+++ C++ Challenges/Calculator.cpp
@@ -1,108 +1,151 @@
-#include <vector>
 #include <iostream>
 
 using std::cout;
 using std::cin;
 using std::endl;
-using std::vector;
 
-void add(&vec,sum){
+//the program continues to add, subtract, divide and multiply until it is stopped. holds sum in memory so that you can do multiple math operation
+// for example add 5 number and divide by 5.
 
-    for (auto n : vec){
-        sum += n;
-    }
-    cout << "sum is " << sum << endl;
-}
-
-void sub(){
-    for(auto n : vec)
-        sum -= n;
-    cout << sum;
-    cout << sum << enld;
-}
-
-void multi(){
-    sum = 1;
-    for (auto n : vec )
-        sum *= n;
-    cout << sum << endl;
-}
-
-void mod(){
-   sum = vec.first
-}
-
-
-int main(){
-    double sum = 0;
-    vector<double> vec;
-    char input;
-    auto inputNumber;
-
-    do
+void add(double *sum){ // ADDITION
+    double x;
+    while (true)
     {
-        cout << "----------------------------------------" << endl;
-        cout << "N - input numbers" << endl;
-        cout << "S - stop input numbers" << endl;
+        cin >> x;
+        *sum +=x;
+        cout << "sum = " << *sum << endl;
+        if(x==0)
+            break;
+    }
+}
+
+void sub(double *sum){ // SUBTRACTION
+    double x;
+    while (true)
+    {
+        cin >> x;
+        *sum -= x;
+        cout << "sum = " << *sum << endl;
+        if(x==0)
+            break;
+    }
+}
+
+void multi(double *sum){ // MULTIPLIER
+    double x,y;
+    if (*sum == 0)
+    {
+        cin >> x;
+        cin >> y;
+            *sum = x*y;
+        cout << "sum = " << *sum << endl;
+    }
+    
+    while (true)
+    {
+        cin >> x;
+        if(x==0)
+            break;
+        *sum *=x;
+        cout << "sum = " << *sum << endl;
+    }
+}
+
+void divide(double *sum){ // DIVIDER
+    double x,y;
+    if (*sum == 0)
+    {
+        cin >> x;
+        cin >> y;
+        if(x >= y)
+            *sum = x/y;
+        else
+            *sum = y/x;
+    }
+
+    while (true)
+    {
+        cin >> x;
+        if(x==0)
+            break;
+        *sum /=x;
+        cout << "sum = " << *sum << endl;
+    }
+}
+
+void mod(){ // MODULUS OF TWO INTEGER
+    int x,y;
+    cout << "please enter first number " << endl;
+    cin >> x;
+    cout << "please enter second number " << endl;
+    cin >> y;
+
+    if(x>=y){ // CHECKS FOR BIGGER NUMBER
+        int mod = x%y;
+        cout << "x % y = " << mod << endl;
+    }
+    else{
+        int mod = y%x;
+        cout << "y % x = " << mod << endl;
+    }
+
+}
+
+void showMenu(){ // MENU TO SHOW HOW TO USE
+    cout << "----------------------------------------" << endl;
         cout << "C - clear numbers" << endl;
         cout << "+ - addition of numbers" << endl;
         cout << "- - subtraction of two numbers" << endl;
-        cout << "x - multiplication of numbers" << endl;
+        cout << "* - multiplication of numbers" << endl;
         cout << "/ - divide the numbers" << endl;
-        cout << "% - modulus of two numbers" << endl;
+        cout << "% - divide the numbers" << endl;
+        cout << "enter '0' (zero) for stop operation" << endl;
         cout << "Q - exit" << endl;
+}
+
+int main(){
+    double sum = 0;
+    char input;
+
+    do
+    {
+        showMenu();
 
         cin >> input;
 
         switch (input)
         {
-            
-        case 'n':
-        case 'N':
-            while (true)
-            {
-                cin >> inputNumber;
-                if(inputNumber == 'S' || inputNumber == 's')
-                    break;
-                vec.push_back(inputNumber);
-            }
-            break;
-
+        
         case 'C':
         case 'c':
-            vec.clear();
             sum = 0;
             break;
 
         case '+':
-            add(&vec);
+            add( &sum);
             break;
 
         case '-':
-
+            sub( &sum);
             break;
 
         case '/':
-
+            divide( &sum);
             break;
 
+        case '*':
+            multi( &sum);
+            break;
+        
         case '%':
-            if(vec.size != 2){
-                cout << "please enter only 2 values" << endl;
-                break;
-            }
+            mod();
             break;
 
         default:
             cout << "please enter valid input" << endl;
-            cin >> input;
             break;
         }
     }while( input != 'Q' && input != 'q' ); // QUIT
-
-
-
-
-
+    cout << "goodbye" << endl;
     return 0;
 }
